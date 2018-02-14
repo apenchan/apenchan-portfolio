d81f52d75bd7191b39aa447fc7a2682ce0d0a883
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2018-02-14 13:20:46 -0500

    did the first page of my new portfolio

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port);
+console.log('=============================');
+console.log('Server running on the new portfolio ' + port);
+console.log('============================='); 
