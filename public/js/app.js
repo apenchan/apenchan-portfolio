bfb1c970d79c48341d8c2c62b686428896a0bfef
anna Penchansky <annapenchansky@annas-MacBook-Pro.local>
2018-02-22 13:51:37 -0500

    writing jquery to get elements to flip on mouseover. Although a bit clunky, I have gotten it so that every mouseover of the img does display the next sibling element. Now I need to fix the toggle/mouseover so that when your mouse is not on the elem, then the img reappears

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,30 @@
 console.log("hello world");
 
+//hacky but works
+$(document).ready(function() {
+  
+    var back = $(".back");//declaring a var for elem back
+    back.hide();//when my document loads, I will hide the elem from sight
+  
+    $(".lang-img").mouseover(function () {//when I hover over ".lang-img"
+      if($(back).css('display') == 'none'){//check to see if back is display none and if so
+        $(this.nextElementSibling).show("inline-block")//then this (which is lang-img) and its next elem (which is .back), will display inline block
+      } else if//otherwise
+        ($('.lang-img').css('display') =='none'){//if .lang-img is display none (because of the mouseover)
+          $(this.nextElementSibling).show("inline-block")//then this(.lang-img) next sibling (which is back), will display inline-block
+      }
+      $(this).toggle()
+    })
+  })
+
+  // $(document).ready(function() {
+//   $(".lang-img").hover(function () {
+//     $(this).css({
+//       ".level.back": "",
+//       'perspective': '100px',
+//       'transform':'rotateX(-180deg)'
+//     })
+//     $('lang-img').toggle()
+//   })
+// })
+
